1194d77206c6c8cba2f2ef69797e712009dcdc5f
= <ryan.murphy@takor.com.au>
2016-11-18 11:51:18 +0800

    Created new blank view Show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,10 +2,22 @@ class VideosController < ApplicationController
 	def index
 	end
 
+	def show
+		@video = Video.find(params[:id])
+	end
+
 	def create
-		@video = Video.new(params[:article])
+		@video = Video.new(video_params)
 
 		@video.save
 		redirect_to @video
 	end
+
+	private
+		def video_params
+			params.require(:video).permit(:title, :text)
+		end
+
+
+
 end
