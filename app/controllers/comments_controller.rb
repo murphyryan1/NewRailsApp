ee6bc9c3e8f240be253cd3ea47f5626dc2e6901b
= <ryan.murphy@takor.com.au>
2016-11-18 19:31:01 +0800

    Add ability to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
 		redirect_to video_path(@video)
 	end
 
+	def destroy
+		@video = Video.find(params[:video_id])
+		@comment = @video.comments.find(params[:id])
+		@comment.destroy
+		redirect_to video_parth(@video)
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,4 @@
 class Video < ApplicationRecord
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 3}
 end
