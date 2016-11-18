6e47b17b40fb2f6acf230377841dd610b5a5bebb
= <ryan.murphy@takor.com.au>
2016-11-18 11:05:41 +0800

    Created videos controller and blank page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
   root 'home#index'
-
 
   resources :articles
   resources :recipes
+  resources :videos
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
